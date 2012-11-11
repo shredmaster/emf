63528512ae4a7b8feca4d6ba354ce59382ef15b1
stevenlin <stevenlin@MACL4553.local>
2012-11-11 16:26:06 -0500

    new files:

--- public/emf.js
+++ public/emf.js
@@ -1,3 +1,24 @@
+var journey = {
+    2009:{
+        event:{
+            title:"",
+            image:[
+                "img/poster/EMF_final_crash_mansion_small.jpg",
+                "img/press/emf2_world_journal.jpeg",
+                "img/press/emf2_world_journal2.jpeg",
+                "http://sphotos-b.xx.fbcdn.net/hphotos-ash3/44885_437495435875_5209589_n.jpg",
+                "http://sphotos-b.xx.fbcdn.net/hphotos-prn1/66651_452800284733_3448640_n.jpg",
+                "http://sphotos-a.xx.fbcdn.net/hphotos-prn1/33738_452800164733_1757714_n.jpg",
+                "http://sphotos-b.xx.fbcdn.net/hphotos-prn1/66370_452799939733_5286931_n.jpg",
+                "http://sphotos-a.xx.fbcdn.net/hphotos-prn1/47001_452915219733_7346339_n.jpg",
+                "http://sphotos-a.xx.fbcdn.net/hphotos-ash3/68553_453239464733_7616237_n.jpg",
+                "http://sphotos-a.xx.fbcdn.net/hphotos-prn1/47001_452915219733_7346339_n.jpg",
+                "http://sphotos-b.xx.fbcdn.net/hphotos-prn1/69828_10150313811140585_1839995_n.jpg"
+            ]
+        }
+    }
+};
+
 $(document).ready(function () {
     // put all your jQuery goodness in here.
     $(".accordion").tabs(
@@ -13,7 +34,7 @@ $(document).ready(function () {
         // use the navigator plugin
     }).navigator({navi:".main_menu"}).data("scrollable");
 
-    var theSize = [1300, 1400, 2100, 5000];
+    var theSize = [1300, 1400, 2100, 1400];
 
     // this callback does the special handling of our "intro page"
     api.onBeforeSeek(function (e, i) {
@@ -66,10 +87,14 @@ $(document).ready(function () {
     });*/
 
     // event page scroller
-    $(".circular_scroll").scrollable({ circular:true }).click(function () {
-        $(this).data("scrollable").next();
+    $(".circular_scroll").scrollable({ circular:true }).click(function (e) {
+        var offset = $(this).offset();
+        var theLeft =  offset.left + $(this).width()/2;
+        if(e.pageX > theLeft) {
+            $(this).data("scrollable").next();
+        } else {
+            $(this).data("scrollable").prev();
+        }
     });
-
-
 });
 
